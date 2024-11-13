a72767ee75ee32728e8e339d49eac86ad755f017
SUGIMOTO Tatsuo <sugi2000@gmail.com>
2024-11-13 14:42:26 +0900

    integrate firebase

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +6,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+    prerender: {
+      handleHttpError: 'warn'  // or 'ignore' if you want to ignore 404s
+    }
 	}
 };
 
